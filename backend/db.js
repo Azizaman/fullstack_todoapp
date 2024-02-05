5fa1905cf0a586540780472694738efd8a65dfc9
Azizaman <110014054+Azizaman@users.noreply.github.com>
2024-02-05 23:53:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://azizamanaaa97:2s3354LUZe0BzSu7@cluster0.tyjfznw.mongodb.net/todo_app');
+mongoose.connect('Add your own mongoDb URL here');
 
 const todoSchema = mongoose.Schema({
     title: String,
